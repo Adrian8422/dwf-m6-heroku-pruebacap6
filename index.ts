52f4e48840b1db824e7362bd7b0f902ef7b5c2b9
Adrian Villegas <manbassman1996@gmail.com>
2022-01-25 15:20:25 -0300

    env vars 2

--- index.ts
+++ index.ts
@@ -13,6 +13,12 @@ app.get("/env", (req, res) => {
     environment: process.env.NODE_ENV,
   });
 });
+///PROXIMO END POINT SE REFIERE LA URL ALGUNA BASE DE DATOS
+app.get("/db-env", (req, res) => {
+  res.json({
+    "db-host": process.env.DB_HOST,
+  });
+});
 app.get("/hola", (req, res) => {
   res.json({
     message: "Hola soy el servidor, heroku",
