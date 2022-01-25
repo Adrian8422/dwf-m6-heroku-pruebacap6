c41fdeb26b67347edd55a60fc8c69d928a63afa8
Adrian Villegas <manbassman1996@gmail.com>
2022-01-25 12:32:09 -0300

    tercer-commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 3000;
 // console.log(process.env.FIREBASE_CONECTION);
 app.get("/hola", function (req, res) {
     res.json({
-        message: "Hola soy el servidor"
+        message: "Hola soy el servidor, heroku"
     });
 });
 app.listen(3500, function () {
--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import * as express from "express";
 const app = express();
-const port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 // console.log(process.env.FIREBASE_CONECTION);
 app.get("/hola", (req, res) => {
   res.json({
