77a7dce8e919f8459f7491a65d4474804f088b03
Adrian Villegas <manbassman1996@gmail.com>
2022-01-25 15:11:22 -0300

    env vars

--- index.ts
+++ index.ts
@@ -1,7 +1,18 @@
 import * as express from "express";
 const app = express();
 const port = process.env.PORT || 3000;
-// console.log(process.env.FIREBASE_CONECTION);
+///CLASICO EN AMBIENTE SE DECLARA LA VARIABLE DE ABAJO PARA LLAMAR A MODO DEVELOPMENT
+// const dev = process.env.NODE_ENV == "development";
+
+// console.log(process.env.NODE_ENV);
+// console.log(process.env.DB_HOST);
+
+// console.log(process.env.NODE_ENV == "development");
+app.get("/env", (req, res) => {
+  res.json({
+    environment: process.env.NODE_ENV,
+  });
+});
 app.get("/hola", (req, res) => {
   res.json({
     message: "Hola soy el servidor, heroku",
